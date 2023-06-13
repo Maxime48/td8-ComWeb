2821ab51fc2fa82dd193d01e9ccce35a87d23329
dekersab3u <dekersab3u@etu.univ-lorraine.fr>
2023-06-13 13:27:50 +0200

    Ex3 functional

--- gallery.js
+++ gallery.js
@@ -1,12 +1,30 @@
 import { loadResource } from './photoloader.js';
 
 let galleryData = [];
+let navigationLinks = {};
 
 export function load(url) {
     return loadResource(url)
         .then(data => {
             galleryData = data.photos;
+            navigationLinks = data.links; // Stockez les liens de navigation
             return galleryData;
         })
         .catch(error => console.error('Erreur lors du chargement de la galerie : ', error));
+}
+
+export function next() {
+    return load('https://webetu.iutnc.univ-lorraine.fr' + navigationLinks.next.href);
+}
+
+export function prev() {
+    return load('https://webetu.iutnc.univ-lorraine.fr' + navigationLinks.prev.href);
+}
+
+export function first() {
+    return load('https://webetu.iutnc.univ-lorraine.fr' + navigationLinks.first.href);
+}
+
+export function last() {
+    return load('https://webetu.iutnc.univ-lorraine.fr' + navigationLinks.last.href);
 }
--- photobox.js
+++ photobox.js
@@ -1,7 +1,23 @@
-import { load } from './gallery.js';
+import { load, next, prev, first, last } from './gallery.js';
 import { display_galerie } from './gallery_ui.js';
 
 document.getElementById("load_gallery").addEventListener("click", () => {
     load('https://webetu.iutnc.univ-lorraine.fr/www/canals5/phox/api/photos/')
         .then(gallery => display_galerie(gallery));
+});
+
+document.getElementById("next_page").addEventListener("click", () => {
+    next().then(gallery => display_galerie(gallery));
+});
+
+document.getElementById("previous_page").addEventListener("click", () => {
+    prev().then(gallery => display_galerie(gallery));
+});
+
+document.getElementById("first_page").addEventListener("click", () => {
+    first().then(gallery => display_galerie(gallery));
+});
+
+document.getElementById("last_page").addEventListener("click", () => {
+    last().then(gallery => display_galerie(gallery));
 });
